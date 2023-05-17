90d1ae4b64bb653d1e072def24a2665c007a203c
Gonlsali <radhita09@gmail.com>
2023-05-17 12:27:10 +0700

    willy kontol

--- src/User.java
+++ src/User.java
@@ -1,6 +1,6 @@
 public class User {
     public static void main(String[] args) {
         System.out.println("Ini adalah class user");
-        System.out.println("noob");
+        System.out.println("willy kontol");
     }
 }
