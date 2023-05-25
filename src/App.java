5b554910aaea21cc99efc09df9615300a43e3968
Gonlsali <radhita09@gmail.com>
2023-05-25 09:49:36 +0700

    Registrasi account, dan Login (uda work, n uda bisa masuk linkedlist dan database)

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.LinkedList;
 public class App {
     Scanner inp = new Scanner(System.in);
+    Account curr;
     public void run(){
         Bank bank = new Bank();
         Connector mySql = new Connector();
@@ -40,6 +41,10 @@ public class App {
         System.out.println("List of account number:");
         bank.displayAccountNumber();
 
+        if (bank.getAccountTotals() == 0){
+            return;
+        }
+
         System.out.print("Choose which account to log in:");
         int menuLogin = inp.nextInt();
 
@@ -47,14 +52,14 @@ public class App {
             System.out.println("Invalid Input!");
             menuLogin(bank);
         } else {
-            Account curr = (Account) bank.listAccount().get(menuLogin - 1);
+            curr = (Account) bank.listAccount().get(menuLogin - 1);
             String pass = curr.getPassword();
 
             System.out.println("Password: ");
             String curPass = inp.next() + inp.nextLine();
 
             if (curPass.equals(pass)){
-                mainMenu(curr);
+                mainMenu(curr, bank);
             } else {
                 System.out.println("Wrong Password!");
                 menuLogin(bank);
@@ -74,10 +79,48 @@ public class App {
         System.out.print("Please deposit some money into your account: ");
         int balance = inp.nextInt();
         Account account = new Account(accountNumber, name, balance, pwd);
+        bank.addAccount(account); // Add to LinkedList
+        bank.addDB(account); // Add to Database
+
+        run();
     }
 
-    public void mainMenu(Account current){
+    public void mainMenu(Account current, Bank bank){
+        System.out.println(
+                "███╗   ███╗███████╗███╗   ██╗██╗   ██╗\n" +
+                "████╗ ████║██╔════╝████╗  ██║██║   ██║\n" +
+                "██╔████╔██║█████╗  ██╔██╗ ██║██║   ██║\n" +
+                "██║╚██╔╝██║██╔══╝  ██║╚██╗██║██║   ██║\n" +
+                "██║ ╚═╝ ██║███████╗██║ ╚████║╚██████╔╝\n" +
+                "╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝ \n");
+        System.out.println("Account Number: " + current.getAccountnumber() + "\n" +
+                "1. Withdraw \n" +
+                "2. Deposit \n" +
+                "3. Transfer \n" +
+                "4. Account Information \n" +
+                "5. Transaction History \n" +
+                "0. Exit");
+        System.out.print("Your Choice: ");
+        int mainMenu = inp.nextInt();
 
+        switch (mainMenu){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 0:
+                System.out.println("Bye-bye");
+                System.exit(0);
+            default:
+                System.out.println("Invalid Input!");
+                break;
+        }
     }
 
 }
